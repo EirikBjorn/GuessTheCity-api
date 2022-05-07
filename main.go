6fc008fd05cdb2fa75ace41850a3cadc087bdccb
Eirik Bjørneseth <eirik_vif@hotmail.com>
2022-05-08 01:07:09 +0200

    local test server

--- main.go
+++ main.go
@@ -40,5 +40,10 @@ func getDataFromJson() []City {
 }
 
 func getCities(c *gin.Context) {
+
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+  c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+  c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+  c.Writer.Header().Set("Access-Control-Allow-Methods", "POST")
 	c.IndentedJSON(http.StatusOK, getDataFromJson())
 }
